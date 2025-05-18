7f38fba49b5c7a21ff95ab1415ae66b17f2ba635
Ousman Habib <uthmmanhabib@gmail.com>
2025-05-18 14:02:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/lib/firebase.ts:5:40
    > Parsing ecmascript source code failed
    >   3 | import { initializeApp, getApps, type FirebaseApp } from "firebase/app";
    >   4 | import { getAuth, type Auth } from "firebase/auth";
    > > 5 | import { getFirestore, type Firestore }_ from "firebase/firestore";
    >     |                                        ^
    >   6 |
    >   7 | // TODO: Add your own Firebase configuration from your Firebase project console
    >   8 | // For Firebase JS SDK v7.20.0 and later, measurementId is optional
    >
    > Expected 'from', got '_'
    >
    >

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
-import { getFirestore, type Firestore }_ from "firebase/firestore";
+import { getFirestore, type Firestore } from "firebase/firestore";
 
 // TODO: Add your own Firebase configuration from your Firebase project console
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
